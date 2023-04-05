0769d0464ce8da45a57e3f4db66e0e921b2ab067
Habisheck pachaiyappa <110193949+Habisheckpachaiyappa@users.noreply.github.com>
2023-04-04 22:08:04 -0700

    Add files via upload

---
+++ assignment day 1/10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int decimal_num, binary_num[32], i = 0;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimal_num);
+
+    while (decimal_num > 0) {
+        binary_num[i] = decimal_num % 2;
+        decimal_num /= 2;
+        i++;
+    }
+
+    printf("Binary number: ");
+    for (int j = i - 1; j >= 0; j--)
+        printf("%d", binary_num[j]);
+
+    return 0;
+}
